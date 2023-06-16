6d31cc5b1b8c4d6e061472c9c4dd71ad85c78f8e
AbimbolaAdemola <abimbolawaris328@gmail.com>
2023-06-16 10:44:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a line of code using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle" );
+	return (0);
+}
