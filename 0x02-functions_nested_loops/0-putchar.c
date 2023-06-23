f6c190dbcedabbda8d6553bb1a43d525d4c4355f
AbimbolaAdemola <abimbolawaris328@gmail.com>
2023-06-23 07:59:32 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /**
- * main - Entry point
+ *  main - Print the word _putchar using the putchar() function
  *
- * Return: Always 0 (success)
- */
+ *  Return: Always 0 (Success)
+ *
+*/
 
 int main(void)
 {
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
-printf("_putchar\n");
-
-return (0);
+	return (0);
 }
 
