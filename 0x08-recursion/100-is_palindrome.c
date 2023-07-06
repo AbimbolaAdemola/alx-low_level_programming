e707646659c4fd178116249fba124140c3303f21
AbimbolaAdemola <abimbolawaris328@gmail.com>
2023-07-06 06:52:36 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,48 +1,44 @@
+#include <stdio.h>
 #include "main.h"
-
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
-
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
- *
- * Return: 1 if it is, 0 it's not
- */
-int is_palindrome(char *s)
-{
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
-}
-
-/**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- *
+ * _strlen_recursion - returns the length of a string.
+ * @s: the string to count
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	if (*s)
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+	return (0);
 }
-
 /**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
+ * finder - helper function for is_palindrome
+ * @str: the string
+ * @len: length of string
+ * @count: counter of recursion
+ * Return: 1 if string is a palindrome, 0 if it is not.
  */
-int check_pal(char *s, int i, int len)
+int finder(char *str, int len, int count)
 {
-	if (*(s + i) != *(s + len - 1))
-		return (0);
-	if (i >= len)
+	if (count >= len)
 		return (1);
-	return (check_pal(s, i + 1, len - 1));
+	if (str[len] == str[count])
+		return (finder(str, len - 1, count + 1));
+	return (0);
+}
+/**
+ * is_palindrome - checks if the string is a palindrome
+ * @s: the string to check
+ * Return: 1 if string is a palindrome, 0 if it is not.
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	int count = 0;
+
+	return (finder(s, len - 1, count));
 }
 
